02e3aa809cd987ad9f9fa1c304b55fa27ea283aa
Aas Irgi <aasirgi1@gmail.com>
2026-04-18 10:25:20 +0700

    Add files via upload

---
+++ ClassAndObject-main/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student 1
+            Students s1 = new Students();
+            s1.name = "Andi";
+            s1.age = 20;
+            s1.studentId = "S001";
+            s1.score = 85;
+
+            s1.displayInfo();
+            s1.checkStatus();
+
+            Console.WriteLine();
+
+            //Student 2
+            Students s2 = new Students();
+            s2.name = "Budi";
+            s2.age = 21;
+            s2.studentId = "S002";
+            s2.score = 60;
+
+            s2.displayInfo();
+            s2.checkStatus();
+        }
+    }
+}
---
+++ ClassAndObject-main/Exercise3/Exercise3/Students.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Students
+    {
+        // Properties
+        public string name;
+        public int age;
+        public string studentId;
+        public double score;
+
+        // display student info
+        public void displayInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Student ID: " + studentId);
+        }
+
+        // fail/pass status
+        public void checkStatus()
+        {
+            if(score >= 75)
+            {
+                Console.WriteLine("Passed");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+}
